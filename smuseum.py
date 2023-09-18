3f52c03b22f3cb078b38286023a280f6ea84141d
Moacyr Ribeiro Blondet <blondet.mr@gmail.com>
2023-09-17 22:47:48 -0300

    Upload da Estrutura do Código e Chamadas da API

--- smuseum.py
+++ smuseum.py
@@ -1,3 +1,7 @@
+# =============================================================================
+""" 1 - Carga Inicial.
+"""
+# =============================================================================
 from flask import redirect, request, Flask, jsonify, make_response
 from flask_openapi3 import OpenAPI, Info, Tag
 from flask_cors import CORS
@@ -13,77 +17,80 @@ import requests
 
 from logger import setup_logger
 
+# ===============================================================================
+""" 2 - Inicializa variáveis de Informações gerais de identificação do serviço.
+"""
+#  ==============================================================================
+info = Info(title="API Search in Museum", version="1.0.0")
+app = OpenAPI(__name__, info=info)
+
+home_tag = Tag(name="Documentação", description="Apresentação da documentação via Swagger.")
+obra_tag = Tag(name="Rota em smuseum", description="Realiza busca de link de imagem de obra de arte")
+doc_tag = Tag(name="Rota em Search in Museum", description="Documentação da API Search in Museum no github")
+
 # ==============================================================================
-""" Inicializa service_name com o nome exclusivo do serviço para fins de geração
-    de arquivo de log.
+""" 3 - Inicializa "service_name" para fins de geração de arquivo de log.
 """
 # ==============================================================================
 service_name = "smuseum"
 logger = setup_logger(service_name)
 
 # ==============================================================================
-""" Informações de identificação, acesso e documentação do serviço
-"""
-# ==============================================================================
-info = Info(title="API Search in Museum", version="1.0.0")
-app = OpenAPI(__name__, info=info)
-
-home_tag = Tag(name="Documentação", description="Apresentação da documentação via Swagger.")
-obra_tag = Tag(name="Rota em smuseum", description="Realiza busca de link de imagem de obra de arte")
-
-# ==============================================================================
-""" Configurações de "Cross-Origin Resource Sharing"
-
-Foi colocado "supports_credentials=False" para evitar possíveis conflitos com
-algum tipo de configuração de browser. Mas não é a melhor recomendação por 
-segurança. Para melhorar a segurança desta API, o mais indicado segue nas 
-linhas abaixo:
-
+""" 4 - Configurações de "Cross-Origin Resource Sharing" (CORS).
+# Foi colocado "supports_credentials=False" para evitar possíveis conflitos com
+# algum tipo de configuração de browser. Mas não é a melhor recomendação por 
+# segurança. Para melhorar a segurança desta API, o mais indicado segue nas 
+# linhas abaixo comentadas.
 #> origins_permitidas = ["Obras de Arte"]
-# Configurando o CORS com suporte a credenciais
+#> Configurando o CORS com suporte a credenciais
 #> CORS(app, origins=origins_permitidas, supports_credentials=True)
 #> CORS(app, supports_credentials=True, expose_headers=["Authorization"])
-# Adicionalmente utilizar da biblioteca PyJWT
-
+#> Adicionalmente utilizar da biblioteca PyJWT
 """
 # ==============================================================================
-
 CORS(app, supports_credentials=False)
 
-# ========================================================================================
-""" Rota /openapi para geração da documentação via Swagger
+# ================================================================================
+""" 5.1 - DOCUMENTAÇÂO: Rota "/" para geração da documentação via Swagger.
 """
-# ========================================================================================
+# ================================================================================
 @app.get('/', tags=[home_tag])
 def home():
     """Redireciona para /openapi/swagger.
     """
     return redirect('/openapi/swagger')
 
-# ==============================================================================
-""" Rota /smuseum
-
-No futuro os "request.args.get" podem ser acrescidos de algumas das variáveis
-inicializadas na parte "Inicialização de Variáveis" neste código.
-
+# ================================================================================
+""" 5.2 - DOCUMENTAÇÂO: Rota "/doc" para documentação via github.
 """
-# ==============================================================================
+# ================================================================================
+@app.get('/doc', tags=[doc_tag])
+def doc():
+    """Redireciona para documentação no github.
+    """
+    return redirect('https://github.com/Moriblo/smuseum')
+
+# ==============================================================================++
+""" 6.1 - Rota "/smuseum" para tratar o fetch de `GET`.
+"""
+# ==============================================================================++
 @app.get('/smuseum', methods=['GET'], tags=[obra_tag],
             responses={"200": SmuseumSchema, "500": ErrorSchema})
 
 def link(query: SmuseumBuscaSchema):
     """Busca link de imagem de obra de arte a partir do nome do Artista e Obra.
     """
-    # Incializa variáveis gerais
-    contador_obras = 0
-    error_code = "200"
-
-    # Lê o valor do parâmetro de consulta 'entrada' da solicitação
+    
+    # ==============================================================================
+    """ 6.1.1 - Lê pela chamada na rota /smuseum os valores para realização da 
+        consulta.
+    """
+    # ==============================================================================
     busca_obra = request.args.get('nome')
     busca_artista = request.args.get('artista')
 
     # ==============================================================================
-    """ Inicialização de Variáveis.
+    """ 6.1.2 - Inicialização de Variáveis.
 
         <museu - Nome do museu>, <museum_url_search - Rota para busca Inicial>
         <museum_url_object - Rota de busca dos objetos retornados na busca inicial>
@@ -103,6 +110,8 @@ def link(query: SmuseumBuscaSchema):
         este museu.
     """
     # ==============================================================================
+    contador_obras = 0
+    error_code = "200"
 
     # Inicia as variáveis para o MET
     museu = "MET"
@@ -114,10 +123,17 @@ def link(query: SmuseumBuscaSchema):
     campo_ID = "objectIDs"
     campo_result = "primaryImage"
 
-    # URL para a pesquisa do artista e coleta dos códigos das obras relacionadas
+    # ==============================================================================
+    """ 6.1.3 - Define URL para a realização da consulta.
+    """
+    # ==============================================================================
     search_url = f'{museum_url_search}{busca_artista}'
 
-    # Realiza a pesquisa e retorna os IDs e total de objetos, caso haja artista
+    # ==============================================================================
+    """ 6.1.4 - Retorna os IDs e total de objetos, CASO HAJA ARTISTA, e trata erro 
+    caso não exista o artista na BD do museu.
+    """
+    # ==============================================================================
     try:
         response = requests.get(search_url)
         response.raise_for_status()
@@ -131,7 +147,11 @@ def link(query: SmuseumBuscaSchema):
     total_objetos = response.json()[campo_total]
     object_ids = response.json()[campo_ID]
     
-    # Verifica se foram encontradas obras para o artista na base do museu
+    # ==============================================================================
+    """ 6.1.5 - Coleta o nome de cada obra, a partir dos IDs associados ao artista,
+        e verifica se esse nome corresponde a obra procurada.
+    """
+    # ==============================================================================
     if (total_objetos != 0):
 
         # Obtém informações sobre cada objeto (obra)
@@ -152,7 +172,7 @@ def link(query: SmuseumBuscaSchema):
             data = json.load(f)
 
         # Inicializa um contador de vezes em que uma dada obra aparece para 
-        # um dado artista. Inicializa um vetor índices que vai coletar os números 
+        # um dado artista. Inicializa um vetor "índices" que vai coletar os números 
         # dos índices dos registros encontrados na lista 'data'.
         indices = []
 
@@ -170,8 +190,6 @@ def link(query: SmuseumBuscaSchema):
                 if busca_obra and DB[campo_obra] and busca_obra.lower() in \
                     str(DB[campo_obra]).lower():
 
-                # if busca_obra.lower() in str(DB[campo_obra]).lower():
-
                     # Incrementa o contador e coleta o número do índice do 
                     # registro na lista.
                     contador_obras += 1
@@ -184,16 +202,19 @@ def link(query: SmuseumBuscaSchema):
             busca_link = data[indices[0]][campo_result]
             logger.warning(f"Msg[1] Dados para registro: Lnk: {busca_link}, Obr: {busca_obra}, Art: {busca_artista}")
         else:
-            # Registra no log o erro por não ter encontrado a obra para o 
-            # artista
+            # Trata erro por não ter encontrado A OBRA para o artista.
             busca_link = f'Erro_[1]: Existem {total_objetos} obras deste artista no museu {museu}. Porém nenhuma como {busca_obra}'
             logger.warning(f"Erro_[1]: Existem {total_objetos} obras deste artista no museu {museu}. Porém nenhuma como {busca_obra}")
     else:
-        #Trata erro quando não encontra nenhuma obra para o artista.
+        #Trata erro quando não encontra NENHUMA obra para o artista.
         busca_link = f'Erro_[2]: Não há obras do artista {busca_artista} no museu {museu}")'
         logger.warning(f"Erro_[2]: Não há obras do artista {busca_artista} no museu {museu}")
         error_code = "404"
 
+    # ==============================================================================
+    """ 6.1.6 - "Monta" o JSON" e retorna o resultado da busca ou a tratativa do erro.
+    """
+    # ==============================================================================
     json_response = ujson.dumps({
         "link": busca_link,
         "obra": busca_obra,
@@ -205,6 +226,9 @@ def link(query: SmuseumBuscaSchema):
 
     return Response(json_response, content_type='application/json'), error_code
 
-
+# ===============================================================================
+""" 7 - Garante a disponibilidade da API em "suspenso".
+"""
+# ===============================================================================
 if __name__ == '__main__':
     app.run(port=5002, debug=True)
